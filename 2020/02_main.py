3fc2652ed6d5b922f853841bcbc2827959f7716d
Shades <Shades4355@gmail.com>
2020-12-02 01:45:49 -0500

    day 2, part 2

--- 2020/02_main.py
+++ 2020/02_main.py
@@ -16,4 +16,25 @@ def oldCorpPasswords(file):
     return("validPasswords: " + str(validPasswords))
 
 ####################
+
+def newCoprPasswords(file):
+    validPasswords = 0
+    for line in file:
+        parsedLine = line.split()
+        yesAndNo = parsedLine[0].split('-')
+        yes = int(yesAndNo[0]) - 1
+        no = int(yesAndNo[1]) - 1
+        string = parsedLine[2]
+        letter = parsedLine[1].strip(':')
+
+        if string[yes] == letter and string[no] != letter:
+            validPasswords += 1
+        elif string[no] == letter and string[yes] != letter:
+            validPasswords += 1
+
+    return("validPasswords: " + str(validPasswords))
+
+
+
 # print(oldCorpPasswords(file))
+print(newCoprPasswords(file))
