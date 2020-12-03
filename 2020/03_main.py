9cf5ea265f9c8845559640e759bf929a917f757e
Shades4355 <51397439+Shades4355@users.noreply.github.com>
2020-12-03 01:10:46 -0500

    Day 3 (#2)
    * day 3, part 1
    
    * day 3, part 2

---
+++ 2020/03_main.py
@@ -0,0 +1,39 @@
+
+def slope(s_row:int, s_column:int, right:int, down:int):
+  file = open("2020/03_data.txt", "r")
+  hill = []
+  for line in file:
+    row = []
+    for character in line:
+      row.append(character)
+    hill.append(row)
+
+  row = s_row
+  column = s_column
+  trees = 0
+  while row < len(hill) - 1:
+    row += down
+    column += right
+    
+    if column >= len(hill[0])-1:
+      column -= len(hill[0])-1
+    
+    position = hill[row][column]
+    if position == "#":
+      trees += 1
+  file.close()
+  return trees
+
+# part 1 #
+# print(slope(0, 0, 3, 1, file))
+
+
+# part 2 #
+slope1 = slope(0, 0, 1, 1)
+slope2 = slope(0, 0, 3, 1)
+slope3 = slope(0, 0, 5, 1)
+slope4 = slope(0, 0, 7, 1)
+slope5 = slope(0, 0, 1, 2)
+
+total_trees = slope1 * slope2 * slope3 * slope4 * slope5
+print(total_trees)
