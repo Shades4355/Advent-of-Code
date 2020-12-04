990d31e6410ed9498c99fb014c1f73de0b47c423
Shades <Shades4355@gmail.com>
2020-12-04 03:47:44 -0500

    2020-Dec-04 03:47

--- 2020/04_main.py
+++ 2020/04_main.py
@@ -1,4 +1,8 @@
+import re
+
 file = open("2020/04_data.txt", "r").read()
+
+# part 1 #
 
 
 def scanner(batchData):
@@ -12,5 +16,35 @@ def scanner(batchData):
     return validPassports
 
 
-# part 1 #
 print(scanner(file))
+
+
+# part 2 #
+def scannerRefined(batchData):
+    passportList = batchData.split('\n\n')
+
+    passport = []
+    for entry in passportList:
+        subsection = []
+        section = re.split(' |\n', entry)
+        for i in section:
+            j = i.split(":")
+            subsection.append(j)
+        passport.append(subsection)
+
+    v = 0
+    for i in passport:
+        for j in i:
+            if j[0] == "byr":
+                if len(j[1]) == 4 and 1920 <= int(j[1]) <= 2002:
+                    v += 1
+            elif j[0] == 'iyr':
+                if len(j[1]) == 4 and 2010 <= int(j[1]) <= 2020:
+                    v += 1
+            elif j[0] == 'eyr':
+                if len(j[1]) == 4 and 2020 <= int(j[1]) <= 2030:
+                    v += 1
+            elif j[0] == 'hgt':
+
+
+scannerRefined(file)
