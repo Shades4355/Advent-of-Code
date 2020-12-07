6d6bb1d56b44bcb66fc3da6adb0f2eb0aa816b07
Shades <Shades4355@gmail.com>
2020-12-07 17:34:01 -0500

    spot check time

--- 2020/07_main.py
+++ 2020/07_main.py
@@ -14,25 +14,41 @@ def bagPuzzle1(file: str):
     for i in bags_in_bags:
         stripped_bag_list = []
 
+        # define 'bag' and remove trainiling 's'
         bag = i.split(' contain ')[0]
-        in_bags = i.split(' contain ')[1].split(', ')
+        if bag.endswith('s'):
+            bag = bag[:len(bag)-1]
 
+        # define stripped_bag_list and remove trailing 's'
+        in_bags = i.split(' contain ')[1].split(', ')
         for i in in_bags:
             stripped_bags = re.sub('[0-9] ', '', i).strip('.')
+            if stripped_bags.endswith('s'):
+                stripped_bags = stripped_bags[:len(stripped_bags)-1]
             stripped_bag_list.append(stripped_bags)
 
+        # make an object with key 'bag' and value 'stripped_bag_list'
         bag_object[bag] = stripped_bag_list
 
-    print(bag_object)
+    # for key in bag_object:
+    #     for i in bag_object[key]:
+    #         if i == "shiny gold bag":
+    #             total += 1
+
+    for i in bag_object:
+        total += getBagContents(i, bag_object)
 
     return total
 
 
-def getBagContents(bags):
-    contents = []
-    # return list of bags
-
-    return contents
+def getBagContents(bag: str, all_bags: object):
+    for i in all_bags[bag]:
+        if i == "shiny gold bag":
+            return 1
+        elif i == "no other bag":
+            return 0
+        else:
+            return getBagContents(i, all_bags)
 
 
 if __name__ == "__main__":
