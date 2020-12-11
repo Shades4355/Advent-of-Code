321d07e19548218d867bc913a10103a977b653f3
Shades <shades355@gmailcom>
2020-12-10 23:59:08 -0500

    2020-Dec-10  23:59

--- 2020/10_main.py
+++ 2020/10_main.py
@@ -27,58 +27,6 @@ def adaptors(file: str):
 # part 2 #
 
 
-def adaptorCombos(file: str):
-    parsed_data = file.split("\n")
-    num_data = [int(num) for num in parsed_data]
-    num_data.append(0)
-    num_data.sort()
-
-    phone = num_data[-1] + 3
-    combos = 0
-    n = 1
-    list_of_combo_lists = []
-    position = 1
-
-    while n < len(num_data):
-        # clear list
-        combo_list = list()
-
-        # has to start at num_data[0]
-        combo_list.append(num_data[0])
-
-        # has to jump by 3 or less
-        if num_data[n] - combo_list[-1] <= 3:
-            combo_list.append(num_data[n])
-        else:
-            return combos
-
-        # incrementally adds numbers to combo_list,
-        # starting at num_data[n + 1]
-        num = n + 1
-        while num < len(num_data) - 1:
-            print("num:", num)
-            # has to jump by 3 or less
-            if num_data[num] - combo_list[-1] <= 3:
-                x = num_data[num]
-                combo_list.append(x)
-                print(combo_list)
-            else:  # invalid list; restart
-                break
-
-            num += 1
-
-        # has to end at phone
-        if num_data[num] - phone <= 3:
-            combos += 1
-            combo_list = list()
-        else:  # invalid list
-            combo_list = list()
-
-        n += 1
-
-    return combos
-
-
 if __name__ == "__main__":
     # print("Part 1:", adaptors(data))
     print("Part 2:", adaptorCombos(test_data))  # 19208
