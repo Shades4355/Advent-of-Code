fb442b7d09500ea1f9280d78e8b663f63006a295
Shades <51397439+Shades4355@users.noreply.github.com>
2020-12-15 16:10:26 -0500

    Day 13 (#15)
    * Day 13, part 1
    
    * Day 13, part 2

---
+++ 2020/13_main.py
@@ -0,0 +1,100 @@
+with open("2020/13_test_data.txt") as input_test_file:
+    test_data = input_test_file.read().split('\n')
+test_leave_time = int(test_data[0])
+test_busses = test_data[1]
+processed_test_busses = test_busses.split(',')
+
+
+with open("2020/13_data.txt") as input_file:
+    data = input_file.read().split('\n')
+leave_time = int(data[0])
+busses = data[1]
+processed_busses = busses.split(',')
+
+
+# Part 1 #
+def bussFinder(departure_time: int, all_busses: list):
+    bus_id = 0  # ID of the bus
+    bus_arrivals = list()
+    active_busses = list()
+
+    for bus in all_busses:
+        if not bus == "x":
+            active_busses.append(int(bus))
+    for bus in active_busses:
+        bus_time = 0  # time the bus actually arrives
+        bus_id = bus
+        while bus_time < departure_time:
+            bus_time += bus_id
+        bus_arrivals.append((bus_id, bus_time))
+
+    shortest_time = departure_time
+    shortest_bus = 0
+    for bus, time in bus_arrivals:
+        # find bus with smallest difference from departure_time
+        time_difference = time - departure_time
+        if time_difference <= shortest_time:
+            shortest_time = time_difference
+            shortest_bus = bus
+
+    return shortest_time * shortest_bus
+
+# Part 2 #
+
+
+class Bus(object):
+    def __init__(self, name, id, minutes_offset):
+        self.name = name
+        self.id = int(id)
+        self.minutes_offset = int(minutes_offset)
+
+
+def timestampHunter(busses: list):
+    bus_list = list()
+    earliest_time = 0
+
+    for num, bus in enumerate(busses):
+        if not bus == "x":
+            bus_list.append(Bus(name="Bus {}".format(bus),
+                                id=bus, minutes_offset=num))
+
+    earliest_time = findTime(bus_list, bus_list[0].id, 0, 0)
+
+    return earliest_time
+
+
+def findTime(busses: list, step: int, n: int, start: int):
+    # ax + bxy + cxyz + [...]
+    print("step", step)
+    print("N", n)
+    print("start", start)
+
+    bus_2 = busses[n + 1]
+
+    answer = 0
+    m = 0
+    while True:
+        time = start + (step * m)
+
+        if (time + bus_2.minutes_offset) % bus_2.id == 0:
+            print("time:", time)
+            print("time + bus_2.minutes_offset", time + bus_2.minutes_offset)
+            print("bus_2.id", bus_2.id)
+            print("~" * 20)
+
+            if n + 2 < len(busses):  # there are busses left
+                # recursive call
+                new_step = step * bus_2.id
+                answer += findTime(busses, new_step, n + 1, time)
+                return answer
+            else:  # no busses left
+                return time
+        m += 1
+
+
+if __name__ == "__main__":
+    # print("Part 1, test:", bussFinder(test_leave_time, processed_test_busses))  # 295
+    print("Part 1:", bussFinder(leave_time, processed_busses))
+    # print("Part 2, test:", timestampHunter(processed_test_busses))  # 1068781
+    print("Part 2:", timestampHunter(processed_busses))
+    pass
