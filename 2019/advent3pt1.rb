780a7a42f0986a03b192346a49023be49e8162a3
Shades <shades355@gmailcom>
2020-12-16 21:52:44 -0500

    2020-Dec-16  21:52 Runs too slow

--- 2019/advent3pt1.rb
+++ 2019/advent3pt1.rb
@@ -1,39 +1,37 @@
-# input = open('2019/advent3pt1_data.txt', "r").read().split(',')
-input = open('2019/advent3pt1_data_test.txt', "r").read().split(',') # 159
+# input = open('2019/advent3pt1_data.txt', "r").read().split("\n")
+input = open('2019/advent3pt1_data_test.txt', "r").read().split("\n") # 6
 
-location = [0,0] # up/down, right/left
+
 visited = Array([])
-crossed = []
+crossed = Array([])
 least = Float::INFINITY
 
-input.each do |full_direction|
-  direction = full_direction[0]
-  distance = full_direction[1..].to_i
-  puts(direction, distance)
-  puts("pre-" + "(" + location.join(", ") + ")")
-  if direction == "U"
-    location[0] += distance
-  elsif direction == "D"
-    location[0] -= distance
-  elsif direction == "R"
-    location[1] += distance
-  elsif direction == "L"
-    location[1] -= distance
-  end
-  puts("post-" +  "(" + location.join(", ") + ")")
 
-  if visited.include?([location])
-    crossed << location
-    puts("True")
-    print("visited: ")
-    print(visited)
-    puts()
-  else
-    visited << location
-    puts("False")
-    print("visited: ")
-    print(visited)
-    puts()
+input.each do |wire|
+  location = [0,0] # up/down, right/left
+  parsed_wire = wire.strip().split(",")
+
+  parsed_wire.each do |full_direction|
+    direction = full_direction[0]
+    distance = full_direction[1..].to_i
+
+    (1..distance).each do
+      if direction == "U"
+        location[0] += 1
+      elsif direction == "D"
+        location[0] -= 1
+      elsif direction == "R"
+        location[1] += 1
+      elsif direction == "L"
+        location[1] -= 1
+      end
+
+      if visited.include?(location)
+        crossed << location.dup
+      else
+        visited << location.dup
+      end
+    end
   end
 
   crossed.each do |crossed_paths|
@@ -41,15 +39,12 @@ input.each do |full_direction|
     if manhattan_distance < least
       least = manhattan_distance
     end
-    puts("least: " + least.to_s)
   end
-  puts("~" * 20)
 end
 
 
 
 
 puts least
-
 
 # too high: 5011
