9a1a7a01476b18ac1b0dade07a8f65668b11c7f9
Shades <shades4355@gmail.com>
2022-12-03 17:36:02 -0500

    advent3pt2 complete

--- 2022/advent3pt2.py
+++ 2022/advent3pt2.py
@@ -1,9 +1,46 @@
 from advent2pt1 import open_file
+from advent3pt1 import priority
+
+# Every set of three lines in your list corresponds to a single group
+def groups_of_three(elves: list) -> list:
+    grouped_elves = []
+    n = 0
+
+    while n < len(elves) - 2:
+        grouped_elves.append([elves[n], elves[n+1], elves[n+2]])
+        n += 3
+    return grouped_elves
 
 
+def find_badge_items(group_of_elves: list) -> str:
+    '''Finds the common item among 3 groups\n
+    Returns item'''
 
-def start():
-    return None
+    badge_list = []
+    
+    for three_group in group_of_elves:
+        for item in three_group[0]:
+            if item in three_group[1] and item in three_group[2]:
+                badge_list.append(item)
+                break
+
+    return badge_list
+
+
+# find priority of all items
+def add_priorities(badge_list: list) -> int:
+    total = 0
+    for badge in badge_list:
+        total += priority(badge)
+    return total
+
+
+def start() -> None:
+    elves = open_file("advent3pt1.txt")
+    group_of_elves = groups_of_three(elves)
+    badge_list = find_badge_items(group_of_elves)
+    total = add_priorities(badge_list)
+    print(total)
 
 #########
 # start #
