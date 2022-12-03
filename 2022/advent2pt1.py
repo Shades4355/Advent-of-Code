b86461da649e66619e32eb0ba4adae206b4d7451
Shades <shades4355@gmail.com>
2022-12-03 14:52:46 -0500

    day 2, part 1 complete

--- 2022/advent2pt1.py
+++ 2022/advent2pt1.py
@@ -1,6 +1,6 @@
 
 
-def open_file(file: str, x:int)->list:
+def open_file(file: str, x: int) -> list:
     '''Open a file in the 2022 folder \n
     Returns a list of first and X+1 values'''
     file = open(f"2022/{file}", "r")
@@ -13,19 +13,21 @@ def open_file(file: str, x:int)->list:
 
     return lines
 
-def compare(opponent_move: str, own_move: str, current_score: int):
+
+def compare(opponent_move: str, own_move: str, current_score: int) -> int:
     '''Compare two throws in Rock-Paper-Scissors \n
     Returns the current score based on win-draw-lose and the shape thrown'''
     total_score = current_score
     if own_move == "X":
-        total_score += rock_throw(opponent_move, current_score)
+        total_score = rock_throw(opponent_move, current_score)
     elif own_move == "Y":
-        total_score += paper_throw(opponent_move, current_score)
+        total_score = paper_throw(opponent_move, current_score)
     elif own_move == "Z":
-        total_score += scissor_throw(opponent_move, current_score)
+        total_score = scissor_throw(opponent_move, current_score)
     return total_score
 
-def rock_throw(opponent_move: str, current_score: int):
+
+def rock_throw(opponent_move: str, current_score: int) -> int:
     '''Throw for Rock \n
     Returns the current score based on win-draw-lose + 1'''
     score = current_score
@@ -40,7 +42,8 @@ def rock_throw(opponent_move: str, current_score: int):
         score += 1 + win()
     return score
 
-def paper_throw(opponent_move: str, current_score: int):
+
+def paper_throw(opponent_move: str, current_score: int) -> int:
     '''Throw for Paper \n
     Returns the current score based on win-draw-lose + 2'''
     score = current_score
@@ -55,7 +58,8 @@ def paper_throw(opponent_move: str, current_score: int):
         score += 2 + lose()
     return score
 
-def scissor_throw(opponent_move: str, current_score: int):
+
+def scissor_throw(opponent_move: str, current_score: int) -> int:
     '''Throw for Scissors \n
     Returns the current score based on win-draw-lose and + 3'''
     score = current_score
@@ -70,37 +74,48 @@ def scissor_throw(opponent_move: str, current_score: int):
         score += 3 + draw()
     return score
 
-def win():
+
+def win() -> int:
     '''Points for winning'''
     return 6
 
-def draw():
+
+def draw() -> int:
     '''Points for a Draw'''
     return 3
 
-def lose():
+
+def lose() -> int:
     '''Points for Losing'''
     return 0
 
-def find_score(rounds: list, current_score=0)->list:
-    '''Find a total score based on a starting score (default 0)'''
-    total_score = current_score
-    m = 0
-    for moves in rounds:
-        total_score += compare(moves[0], moves[1], total_score)
-        m += 1
-    return [total_score, m]
 
-def validate_answer(total_score, total_turns)->None:
+def find_score(rounds: list, current_score=0) -> list:
+    '''Find a total score based on a starting score (default 0)\n
+    Returns a list of "total score" and "number of rounds"'''
+    total_score = current_score
+    num_of_rounds = 0
+    for opponent_move, own_move in rounds:
+        total_score = compare(opponent_move, own_move, total_score)
+        num_of_rounds += 1
+    return [total_score, num_of_rounds]
+
+
+def validate_answer(total_score, total_turns) -> None:
     '''Validates results from Rock-Paper-Scissors competition\n
     Returns "Answer too high!" if the input score is higher than that of a perfect game\n
     Returns "Answer too low!" if the input score is lower than a lose every round'''
     if total_score > total_turns * (6+3):
         print("Answer too high!")
+        print(total_score)
+        print("Matches Played:", total_turns)
     elif total_score < total_turns * 1:
         print("Answer too low!")
+        print(total_score)
+        print("Matches Played:", total_turns)
     else:
         print("score:", total_score)
+        print("Matches Played:", total_turns)
 
 
 #########
@@ -111,5 +126,3 @@ if __name__ == "__main__":
     turns = open_file("advent2pt1.txt", 2)
     total_score, total_turns = find_score(turns)
     validate_answer(total_score, total_turns)
-    
-    
