5108bea058ad006ae5fdacb0140927516fd2df72
Shades <shades4355@gmail.com>
2022-12-04 02:48:56 -0500

    advent4pt1 WIP2

--- 2022/advent4pt1.py
+++ 2022/advent4pt1.py
@@ -14,15 +14,34 @@ def elven_pairs(all_elves: list) -> list:
                 
 
 # compare cleaning spaces
-def 
+def compare(elf_one: list, elf_two: list) -> bool:
+    '''Compare to ranges\n
+    Return True if one range is fully in another'''
+    index_one = elf_one.index("-")
+    range_one = [elf_one[0:index_one], elf_one[index_one:len(elf_one)]]
+    index_two = elf_two.index("-")
+    range_two = [elf_two[0:index_two], elf_two[index_two:len(elf_one)]]
+
+    if range_two[0] <= range_one[0] and range_one[1] <= range_two[1]:
+        return True
+    elif range_one[0] <= range_two[0] and range_two[1] <= range_one[1]:
+        return True
+    return False
+
+
 
 # In how many assignment pairs does one range fully contain the other?
 
 def start():
     all_elves = open_file("advent4pt1.txt")
     paired_elves = elven_pairs(all_elves)
+    total = 0 
 
     for elf_one, elf_two in paired_elves:
+        if compare(elf_one, elf_two):
+            total += 1
+
+    print("Total:", total)
 
 
 #########
@@ -31,3 +50,5 @@ def start():
 
 if __name__ == "__main__":
     start()
+
+    # 529 = Too high
