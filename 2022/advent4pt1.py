eaa0e56000ee816d1d247d69d9d5f9d96691ee27
Shades <shades4355@gmail.com>
2022-12-04 13:13:50 -0500

    Documentatio cleanup

--- 2022/advent4pt1.py
+++ 2022/advent4pt1.py
@@ -13,10 +13,10 @@ def elven_pairs(all_elves: list) -> list:
     return paired_elves
                 
 
-# compare cleaning spaces
-def compare(elf_one: list, elf_two: list) -> bool:
-    '''Compare to ranges\n
+def compare(elf_one: str, elf_two: str) -> bool:
+    '''Compare two ranges\n
     Return True if either range is fully in the other'''
+
     index_one = elf_one.index("-")
     range_one = [elf_one[0:index_one], elf_one[index_one + 1:len(elf_one)]]
     index_two = elf_two.index("-")
@@ -29,17 +29,18 @@ def compare(elf_one: list, elf_two: list) -> bool:
     return False
 
 
-
-# In how many assignment pairs does one range fully contain the other?
-
-def start():
-    all_elves = open_file("advent4pt1.txt")
-    paired_elves = elven_pairs(all_elves)
-    total = 0 
+def count(paired_elves: list) -> int:
+    total = 0
 
     for elf_one, elf_two in paired_elves:
         if compare(elf_one, elf_two):
             total += 1
+    return total
+
+def start():
+    all_elves = open_file("advent4pt1.txt")
+    paired_elves = elven_pairs(all_elves)
+    total = count(paired_elves)
 
     print("Total:", total)
 
