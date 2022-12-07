d0825dee4afa09b6bb720e69ac07bdff08c9ef4b
Shades <51397439+Shades4355@users.noreply.github.com>
2022-12-07 18:46:02 -0500

    Advent6pt2 (#32)
    * improved advent6pt1 to be more versitile
    
    * advent day 6 part 2 complete

--- 2022/advent6pt2.py
+++ 2022/advent6pt2.py
@@ -1,19 +1,15 @@
 from advent2pt1 import open_file
+from advent6pt1 import *
 
 
-def split_data(raw_data: str) -> list:
-    data_list = []
-    
-    for letter in raw_data:
-        data_list.append(letter)
-    return data_list
-
-
-def read_data(data_list: list) -> str:
+def read_data(data_list: list, length: int) -> str:
     '''Takes in a list of 'random' letters; outputs the first instance of 4 concurrent, unique letters'''
 
     for i in range(0, len(data_list), 1):
-        data_chunk = [data_list[i], data_list[i + 1], data_list[i + 2], data_list[i + 3]]
+        data_chunk = []
+        for j in range(length):
+            datum = data_list[i + j]
+            data_chunk.append(datum)
 
         if len(data_chunk) == len(set(data_chunk)):
             return "".join(data_chunk)
@@ -22,17 +18,10 @@ def read_data(data_list: list) -> str:
     exit()
 
 
-def find_index(data_stream: list, data_chunk: str) -> int:
-    '''Finds the index of (the last character in a string) in a list\n
-    Returns an Integer'''
-
-    return "".join(data_stream).find(data_chunk) + len(data_chunk)
-
-
 def start():
     raw_data_stream = open_file("advent6.txt")
     data_stream = split_data(raw_data_stream[0])
-    data_chunk = read_data(data_stream)
+    data_chunk = read_data(data_stream, 14)
     index = find_index(data_stream, data_chunk)
 
     print(index)
