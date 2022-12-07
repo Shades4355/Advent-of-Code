90d487a93db79feeb3986c8d0c2745d0655f8b0f
Shades <shades4355@gmail.com>
2022-12-07 18:22:31 -0500

    day 6 part 1, WIP

---
+++ 2022/advent6pt1.py
@@ -0,0 +1,45 @@
+from advent2pt1 import open_file
+
+
+def split_data(raw_data: str) -> list:
+    data_list = []
+    
+    for letter in raw_data:
+        data_list.append(letter)
+    return data_list
+
+
+def read_data(data_list: list) -> str:
+    '''Takes in a list of 'random' letters; outputs the first instance of 4 concurrent, unique letters'''
+
+    for i in range(0, len(data_list), 1):
+        data_chunk = [data_list[i], data_list[i + 1], data_list[i + 2], data_list[i + 3]]
+        print(f"{i}:", data_chunk) # TODO: delete
+        if len(data_chunk) == len(set(data_chunk)):
+            return "".join(data_chunk)
+
+
+def find_index(data_stream: list, data_chunk: str) -> int:
+    '''Finds the index of a string in a list\n
+    Returns an Integer'''
+
+    return "".join(data_stream).find(data_chunk)
+
+
+def start():
+    raw_data_stream = open_file("advent6.txt")
+    data_stream = split_data(raw_data_stream[0])
+    data_chunk = read_data(data_stream)
+    index = find_index(data_stream, data_chunk)
+
+    print(index)
+
+#########
+# start #
+#########
+
+if __name__ == "__main__":
+    start()
+
+    # 1540 = Too Low
+    # 1541 = Too Low
