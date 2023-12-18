5881e411525be756249c29d458d55ae558ebd7ac
Shades <shades4355@gmail.com>
2023-12-17 21:50:59 -0500

    Day 3, pt 1 (2d list formed)

--- 2023/advent3pt1.py
+++ 2023/advent3pt1.py
@@ -1,28 +1,42 @@
-from advent2pt1 import get_input
+def get_input(location:str):
+    array = []
+    file = open(location, "r")
+
+    for line in file:
+        array.append(line.strip())
+    
+    return array
 
 
 def two_d_ify(engine_list:list):
-    max_col = len(engine_list[0])
     max_row = len(engine_list)
-    
-    engine_array = [[0]*max_col]*max_row
+    max_col = 0
+    column = 0
+
+    for i in range(0, len(engine_list)):
+        if max_col < len(engine_list[column]):
+            max_col = len(engine_list[column])
+        column += 1
+
+    engine_array = [["."]*max_col]*max_row
 
     row = 0
-    for line in engine_list:
+    while row < max_row:
         col = 0
-        for cha in line:
-            print("cha:", cha) # TODO: delete
+        while col < max_col:
+            print("cha:", engine_list[row][col])  # TODO: delete
             print("row:", row, "col:", col) # TODO: delete
-            engine_array[row][col] = cha
+            engine_array[row][col] = engine_list[row][col]
             col += 1
         row += 1
 
     return engine_array
 
+
 def start():
     engine_2d_list = []
 
-    engine_list = get_input()
+    engine_list = get_input("advent3input.txt")
     engine_2d_list = two_d_ify(engine_list)
 
     print(engine_2d_list)
