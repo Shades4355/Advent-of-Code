715fceaf1951af82ac4383fd50487190297779d3
Shades <51397439+Shades4355@users.noreply.github.com>
2023-12-23 18:09:08 -0500

    2023 day3pt2 refinement (#42)
    * day 3, pt 2 (complete)

--- advent_2023/day_3/advent3pt2.py
+++ advent_2023/day_3/advent3pt2.py
@@ -1,10 +1,10 @@
-from advent3pt1 import get_input, two_d_ify, find_max_column, test_num
+from advent3pt1 import get_input, two_d_ify, test_num
 
 
 def find_gear(two_d_list:list):
     '''Takes in a 2D list and outputs the total of the gear ratios'''
     max_rows = len(two_d_list)
-    max_cols = find_max_column(two_d_list)
+    max_cols = len(two_d_list[0])
     gear_ratios = 0
 
     # check each cha to see if it's an *
@@ -19,34 +19,38 @@ def find_gear(two_d_list:list):
     return gear_ratios
 
 
-
 def find_gear_ratio(two_d_list:list, row:int, col:int):
     '''Takes in a 2D list with the row and column of a gear and outputs that gear's gear ratio'''
     total = 1    
-    max_col = find_max_column(two_d_list)
     max_row = len(two_d_list)
+    max_col = len(two_d_list[0])
     r = -1
-    str_num = ""
 
-    while r < 2: # Unit Test fails when these While loops is replaced with a For loops
+    # TODO: fix - gets stuck in infinite loop when encountering a 1 digit number
+    # (ex: two_d_list[44][45] = "9" in ".9.")
+    while r < 2: # Unit Test fails when these While loops is replaced with For loops
         c = -1
+
+        # TODO: find_num is getting repeatedly called on the same coordinates (row: 44, col: 45)
         while c < 2:
-            if 0 <= row + r < max_row and 0 <= col + c < max_col:
-                # Test if two_d_list[row + r][col + c] is a number; returns True or False
-                if test_num(two_d_list[row + r][col + c]):
-                    # If the above is a number, iterate through it to find where it starts and how long it is
-                    num_start, num_length = find_num(two_d_list, row + r, col + c)
-                    # set 'c' to the cha after the number, so we don't accidentally read the same number multiple times
-                    c = num_start + num_length
-                    for i in range(0, num_length):
-                        # create a str of the number
-                        str_num += f"{two_d_list[row + r][num_start + i]}"
-                    if test_num(str_num): # test that the str was created correctly
-                        total *= int(str_num) # multiply all gear ratios for this one gear together
-                    else:
-                        raise Exception("test_num failed unexpectedly")
-                    str_num = ""
-            c += 1
+            if 0 <= row + r < max_row and 0 <= col + c < max_col and test_num(two_d_list[row + r][col + c]):
+                # If the above is a number, iterate through it to find where it starts and how long it is
+                start_offset, num_length = find_num(two_d_list, row + r, col + c)
+
+                str_num = ""
+                for i in range(0, num_length):
+                    # create a str of the number
+                    str_num += str(two_d_list[row + r][col + c + start_offset + i])
+
+                if test_num(str_num): # test that the str was created correctly
+                    total *= int(str_num) # multiply both gear ratios for this one gear together
+                else:
+                    raise Exception("test_num failed unexpectedly\nERROR:", str_num)
+
+                # set 'c' to the cha after the number, so we don't accidentally read the same number multiple times
+                c += start_offset + num_length
+            else:
+                c += 1
         r += 1
 
     # return total gear ratio for this one gear
@@ -55,59 +59,54 @@ def find_gear_ratio(two_d_list:list, row:int, col:int):
 
 def find_num(two_d_list:list, row:int, col:int):
     '''Takes in a 2D list and a position ([row][col]) of a number;
-    outputs the position of the beginning of the number, and the number's length'''
+    outputs the offset of the beginning of the number, and the number's length'''
     left_looping = True
-    begin = -1
-    end = 1
-    max_col = find_max_column(two_d_list)
+    begin_offset = -1
+    max_col = len(two_d_list[0])
 
     # look for the beginning of the number
     while left_looping:
-        if (col + begin) >= 0:
-            if test_num(two_d_list[row][col + begin]):
-                begin -= 1
-            else:
-                left_looping = False
-                begin += 1
+        if (col + begin_offset) >= 0 and test_num(two_d_list[row][col + begin_offset]):
+            begin_offset -= 1
         else:
             left_looping = False
-            begin += 1
+            begin_offset += 1
 
     # Look for the end of the number
+    end_offset = 1
     right_looping = True
     while right_looping:
-        if (col + end) < max_col:
-            if test_num(two_d_list[row][col + end]):
-                end += 1
-            else:
-                right_looping = False
-                end -= 1
+        if (col + end_offset) < max_col and test_num(two_d_list[row][col + end_offset]):
+            end_offset += 1
         else:
             right_looping = False
-            end -= 1
 
-    num_length = end - begin + 1
+    num_length = end_offset - begin_offset
 
     # return the position of the beginning of the number and the number's length
-    return [col + begin, num_length]
+    return [begin_offset, num_length]
 
 
 def test_gear(two_d_list:list, row:int, col:int):
-    '''Takes in a 2D list and the position of a gear; 
-    returns True if exactly 2 numbers are found adjacent/diagonal to it, otherwise returns False'''
-    max_col = find_max_column(two_d_list)
+    '''Takes in a 2D list and the position of a gear;
+    returns True if exactly 2 numbers are found adjacent/diagonal
+    to it, otherwise returns False'''
+    max_col = len(two_d_list[0])
     check = 0
     r = -1
 
     while r < 2:
         c = -1
         while c < 2:
-            if 0 <= row + r < len(two_d_list) and 0 <= col + c < max_col:
-                if test_num(two_d_list[row + r][col + c]):
-                    num_start, num_length = find_num(two_d_list, row + r, col + c)
-                    check += 1
-                    c = num_start + num_length
-            c += 1
+            if 0 <= row + r < len(two_d_list) and 0 <= col + c < max_col and test_num(two_d_list[row + r][col + c]):
+                start_offset, num_length = find_num(two_d_list, row + r, col + c)
+                check += 1
+                if start_offset + num_length <= 0:
+                    c += 1
+                else:
+                    c += start_offset + num_length
+            else:
+                c += 1
         r += 1
 
     if check == 2:
