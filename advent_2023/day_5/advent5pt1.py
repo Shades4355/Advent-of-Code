dc19f0332ede21f1e263d61f2af6ef7d194470bc
Shades <shades4355@gmail.com>
2023-12-25 18:17:46 -0500

    Day 5 pt 2 (WIP)
    * created and tested several functions

--- advent_2023/day_5/advent5pt1.py
+++ advent_2023/day_5/advent5pt1.py
@@ -1,6 +1,6 @@
 
 
-def get_input(location):
+def get_input(location:str):
     '''Takes in a file; outputs a 1D list'''
 
     array = []
@@ -14,7 +14,7 @@ def get_input(location):
     return array
 
 
-def parse_input(file:list): # TODO: investigate why this is failing
+def parse_input(file:list):
     '''Takes in a 1D list; outputs a dictionary'''
     dictionary = {}
     key = ""
@@ -24,30 +24,70 @@ def parse_input(file:list): # TODO: investigate why this is failing
         # first line gets parsed different from the rest
         if line.find("seeds") > -1:
             key = line.split(": ")[0]
-            value = line.split("")[1]
-            dictionary[key] = value.split()
+            value = line.split(": ")[1]
+            dictionary[key] = []
+            for num in value.split():
+                dictionary[key].append(int(num))
         # for 'map' lines, use line as new key
         elif line.find("map") > -1:
             key = line
             iteration = 0
-            dictionary[key] = []
+            dictionary[key] = {}
         # ignore empty lines
         elif line == "":
             continue
         # for numeric lines, add list of numbers to relevant map
         else:
-            dictionary[key][iteration].append(line.split())
+            dictionary[key][str(iteration)] = []
+            for num in line.split():
+                dictionary[key][str(iteration)].append(int(num))
             iteration += 1
 
     return dictionary
 
 
-def start(location):
-    file = get_input(location)
+def get_seed_list(highest:int):
+    all_seeds = []
+
+    for i in range(0, highest + 1):
+        all_seeds.append(i)
+
+    return all_seeds
+
+
+def find_last_seed(map:dict):
+    last_seed = 0
+
+
+    for key in map:
+        if key == "seeds":
+            for num in map[key]:
+                if num > last_seed:
+                    last_seed = num
+        else:
+            for map_list in map[key]:
+                # [88, 18, 7]
+                nums = map[key][map_list]
+                if nums[0] + nums[2] > last_seed:
+                    last_seed = nums[0] + nums[2]
+                if nums[1] + nums[2] > last_seed:
+                    last_seed = nums[1] + nums[2]
+    
+    return last_seed
+
+
+def start(location:str):
+    answer = 0
+    parsed_input = parse_input(get_input(location))
+    last_seed = find_last_seed(parsed_input)
+    seed_list = get_seed_list(last_seed)
+
+
+    return answer
 
 
 #########
 # start #
 #########
 if __name__ == "__main__":
-    start("advent5input.txt")
+    print(start("advent5input.txt"))
--- advent_2023/day_5/day5_test.py
+++ advent_2023/day_5/day5_test.py
@@ -1,5 +1,5 @@
 import unittest
-from advent5pt1 import get_input, parse_input
+from advent5pt1 import get_input, parse_input, start, get_seed_list, find_last_seed
 
 
 class TestAdventDay5pt1(unittest.TestCase):
@@ -45,29 +45,48 @@ class TestAdventDay5pt1(unittest.TestCase):
         self.assertEqual(get_input(self.testInput), file)
 
     def test_parse_input(self):
+        self.maxDiff = 50
         file = {
-            "seeds": ["79", "14", "55", "13"],
-            "seed-to-soil map": [["50", "98", "2"],
-                                  ["52", "50", "48"]],
-            "soil-to-fertilizer map": [["0", "15", "37"],
-                                        [],
-                                        []],
-            "fertilizer-to-water map": [[],
-                                        [],
-                                        [],
-                                        []],
-            "water-to-light map": [[],
-                                   []],
-            "light-to-temperature map": [[],
-                                        [],
-                                        []],
-            "temperature-to-humidity map": [[],
-                                            []],
-            "humidity-to-location map":  [[],
-                                          []]
+            "seeds": [79, 14, 55, 13],
+            "seed-to-soil map:": {"0": [50, 98, 2],
+                                  "1": [52, 50, 48]},
+            "soil-to-fertilizer map:": {"0": [0, 15, 37],
+                                        "1": [37, 52, 2],
+                                        "2": [39, 0, 15]},
+            "fertilizer-to-water map:": {"0": [49, 53, 8],
+                                        "1": [0, 11, 42],
+                                        "2": [42, 0, 7],
+                                        "3": [57, 7, 4]},
+            "water-to-light map:": {"0": [88, 18, 7],
+                                   "1": [18, 25, 70]},
+            "light-to-temperature map:": {"0": [45, 77, 23],
+                                        "1": [81, 45, 19],
+                                        "2": [68, 64, 13]},
+            "temperature-to-humidity map:": {"0": [0, 69, 1],
+                                            "1": [1, 0, 69]},
+            "humidity-to-location map:":  {"0": [60, 56, 37],
+                                          "1": [56, 93, 4]}
         }
         
-        self.assertEqual(get_input(self.testInput), file)
+        self.assertEqual(parse_input(get_input(self.testInput)), file)
+
+    def test_get_seed_list(self):
+        answer = [i for i in range(0, 100)]
+        parsedInput = parse_input(get_input(self.testInput))
+        seed_list = get_seed_list(99)
+
+        self.assertEqual(seed_list, answer)
+
+    def test_find_last_seed(self):
+        answer = 100
+        parsedInput = parse_input(get_input(self.testInput))
+
+        self.assertEqual(find_last_seed(parsedInput), answer)
+
+    def test_start(self):
+        answer = 35
+
+        self.assertEqual(start(self.testInput), answer)
 
 
 #########
