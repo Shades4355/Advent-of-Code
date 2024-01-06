967fb900e871750933f76c551bb67a6cc59a25f3
Shades <shades4355@gmail.com>
2024-01-05 20:54:14 -0500

    Added comments to start()

--- advent_2023/day_6/advent6pt1.py
+++ advent_2023/day_6/advent6pt1.py
@@ -20,6 +20,11 @@ def get_input(location):
 def start(file):
     parsed_info = get_input(file)
 
+    # determine number of ways to win
+    # record number of ways to win each race
+    # multiply number of ways together
+    # Return solution
+
     return "hi"
 
 
