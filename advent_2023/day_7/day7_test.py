565d18daa68ab0acce6b092f50b48dd4dc2bdecc
Shades <shades4355@gmail.com>
2024-01-05 22:17:19 -0500

    day 7 pt 1 started

---
+++ advent_2023/day_7/day7_test.py
@@ -0,0 +1,13 @@
+import unittest
+from advent7pt1 import start as start_pt1, get_input
+
+class TestAdventDay7(unittest.TestCase):
+    test_input = "day7input_test.txt"
+
+
+
+#########
+# start #
+#########
+if __name__ == "__main__":
+    unittest.main()
