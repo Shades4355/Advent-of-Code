9bd7e7528f680eb0a6b189c615d23efd7fde1aa2
Shades <shades4355@gmail.com>
2024-01-09 21:31:45 -0500

    added follow_directions() function and tests
    * all tests pass

--- advent_2023/day_8/advent8pt1.py
+++ advent_2023/day_8/advent8pt1.py
@@ -21,22 +21,46 @@ def parse_input(file:list):
         directions.append(cha)
 
     dictionary["directions"] = directions
+    dictionary["start"] = file[2].split(" = ")[0]
 
     for i in range(2, len(file)):
         key, value = file[i].split(" = ")
-        lv, rv = value.strip().strip("(").strip(")").split(", ")
-        
-        dictionary[key] = [lv, rv]
+        dictionary[key] = value.strip().strip("(").strip(")").split(", ")
 
     return dictionary
+
+
+def follow_directions(dictionary:dict):
+    steps = 0
+    placement = ""
+
+    placement = dictionary["start"]
+
+    while not placement == "ZZZ":
+        for direction in dictionary["directions"]:
+            if direction == "L":
+                placement = dictionary[placement][0]
+            elif direction == "R":
+                placement = dictionary[placement][1]
+            else:
+                raise Exception("Unexpected direction:", direction)
+
+            steps += 1
+
+            if placement == "ZZZ":
+                break
+            elif placement == dictionary[placement][0] and placement == dictionary[placement][1]:
+                raise Exception("Infinite Loop found at:", placement)
+    
+    return steps
+
 
 
 def start(location:str):
     file = get_input(location)
     parsed_file = parse_input(file)
-    
-    
-    return "hi"
+
+    return follow_directions(parsed_file)
 
 
 #########
--- advent_2023/day_8/day8_test.py
+++ advent_2023/day_8/day8_test.py
@@ -1,10 +1,40 @@
 import unittest
-from advent8pt1 import start as start_pt1, get_input, parse_input
+from advent8pt1 import start as start_pt1, get_input, parse_input, follow_directions
 
 
 class TestDay8(unittest.TestCase):
     test_data1 = "day8input_test.txt"
     test_data2 = "day8input_test2.txt"
+
+    def test_follow_directions(self):
+        def test_1(self):
+            file = {
+                "directions": ["R", "L"],
+                "start": "AAA",
+                "AAA": ["BBB", "CCC"],
+                "BBB": ["DDD", "EEE"],
+                "CCC": ["ZZZ", "GGG"],
+                "DDD": ["DDD", "DDD"],
+                "EEE": ["EEE", "EEE"],
+                "GGG": ["GGG", "GGG"],
+                "ZZZ": ["ZZZ", "ZZZ"]
+            }
+            answer = 2
+
+            return self.assertEqual(follow_directions(file), answer)
+        
+        def test_2(self):
+            file = {"directions": ["L", "L", "R"],
+                    "start": "AAA",
+                    "AAA": ["BBB", "BBB"],
+                    "BBB": ["AAA", "ZZZ"],
+                    "ZZZ": ["ZZZ", "ZZZ"]}
+            answer = 6
+
+            return self.assertEqual(follow_directions(file), answer)
+        
+        test_1(self)
+        test_2(self)
 
     def test_get_input(self):
         def test_1(self):
@@ -25,12 +55,12 @@ class TestDay8(unittest.TestCase):
     def test_parse_input(self):
         file = ["LLR\n", "\n", "AAA = (BBB, BBB)\n", "BBB = (AAA, ZZZ)\n", "ZZZ = (ZZZ, ZZZ)"]
         answer = {"directions": ["L", "L", "R"],
+                  "start": "AAA",
                   "AAA": ["BBB", "BBB"],
                   "BBB": ["AAA", "ZZZ"],
                   "ZZZ": ["ZZZ", "ZZZ"]}
 
         self.assertEqual(parse_input(file), answer)
-        
 
     def test_start_pt1(self):
         def test_1(self):
