7fab8a4f8615483b786197bc0368de52cea14d03
Shades <shades4355@gmail.com>
2024-01-11 14:20:16 -0500

    created get_input() function and test
    * created parse_input() function and test
    
    * created start() test; start() function is WIP

--- advent_2023/day_9/advent9pt1.py
+++ advent_2023/day_9/advent9pt1.py
@@ -1,9 +1,41 @@
 
+def get_input(location:str):
+    '''parse txt file into a list'''
+    output_file = []
+
+    file = open(location, "r")
+
+    for line in file:
+        output_file.append(line.strip())
+
+    file.close()
+
+    return output_file
 
 
+def parse_input(file:list):
+    answer = []
+    str_answer = []
 
+    for i in range(0, len(file)):
+        answer.append([])
+        answer[i] = []
+        str_answer.append([])
+        str_answer[i] = file[i].split()
+        for num in str_answer[i]:
+            answer[i].append(int(num))
+
+    return answer
+
+
+def start(location:str):
+    answer = 0
+    file = parse_input(get_input(location))
+
+    return answer
 
 
 #########
 # Start #
 #########
+print(start("day9input.txt"))
--- advent_2023/day_9/day9_test.py
+++ advent_2023/day_9/day9_test.py
@@ -1,11 +1,32 @@
 import unittest
+from advent9pt1 import start as start_pt1, get_input as get_input_pt1, parse_input as parse_input_pt1
 
 class TestDay9(unittest.TestCase):
     test_data = "day9input_test.txt"
 
+    def test_get_input_pt1(self):
+        location = self.test_data
+        answer = ["0 3 6 9 12 15",
+                  "1 3 6 10 15 21",
+                  "10 13 16 21 30 45"]
+        
+        self.assertEqual(get_input_pt1(location), answer)
 
+    def test_parse_input_pt1(self):
+        file = ["0 3 6 9 12 15",
+                  "1 3 6 10 15 21",
+                  "10 13 16 21 30 45"]
+        answer = [[0, 3, 6, 9, 12, 15],
+                  [1, 3, 6, 10, 15, 21],
+                  [10, 13, 16, 21, 30, 45]]
 
+        self.assertEqual(parse_input_pt1(file), answer)
 
+    def test_start_pt1(self):
+        file = self.test_data
+        answer = 114
+
+        self.assertEqual(start_pt1(file), answer)
 
 #########
 # Start #
