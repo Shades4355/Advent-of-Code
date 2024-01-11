b29ffc81f97682185ff3d172559a5c4e5aaefcfc
Shades <shades4355@gmail.com>
2024-01-11 13:58:49 -0500

    day 9 pt 1 - files created

---
+++ advent_2023/day_9/day9_test.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestDay9(unittest.TestCase):
+    test_data = "advent9input_test.txt"
+
+
+
+
+
+#########
+# Start #
+#########
+if __name__ == "__main__":
+    unittest.main()
