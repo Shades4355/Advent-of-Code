e302262cc2e37215a1700da86539e8c94d7b6f11
Shades <shades4355@gmail.com>
2024-01-10 19:44:18 -0500

    added missing comments
    * cleaned up 'while looping' line to be 'while true'

--- advent_2023/day_8/advent8pt2.py
+++ advent_2023/day_8/advent8pt2.py
@@ -2,22 +2,25 @@ from advent8pt1 import get_input
 
 
 def all_end_in_z(placements:list):
+    '''Checks if a list of values all end in "Z"'''
     for num in placements:
         if not num[-1] == "Z":
             return False
+
     return True
 
 
 def follow_directions(file:dict):
+    '''Takes in a dictionary containing starting values and L/R directions; outputs the number of steps needed to reach a situation where all current values end in "Z"'''
     steps = 0
     placement = []
+    new_placement = []
 
     for num in file["start"]:
         placement.append(num)
+        new_placement.append(num)
 
-    looping = True
-    while looping:
-        new_placement = placement
+    while True:
         for direction in file["directions"]:
             for i in range(0, len(placement)):
                 if direction == "L":
@@ -33,12 +36,14 @@ def follow_directions(file:dict):
             if all_end_in_z(placement):
                 return steps
 
+            # tests for infinite loops; ex "ZZZ" = ("ZZZ", "ZZZ")
             for i in range(0, len(placement)):
                 if placement[i] == file[placement[i]][0] and placement[i] == file[placement[i]][1]:
                     raise Exception("Infinite Loop found at:", placement[i], placement)
 
 
 def parse_input(file:list):
+    '''Takes in a list; outputs a dictionary with a "directions" key, a "start" key, and a key for each value'''
     dictionary = {}
     directions = []
     
@@ -58,6 +63,7 @@ def parse_input(file:list):
 
 
 def start(location:str):
+    '''Starts the program. Takes in the location of a file; outputs the prompt answer'''
     file = get_input(location)
     parsed_file = parse_input(file)
 
