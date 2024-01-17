3c33ace20f3b3d669188cc0aaee742c88b5f1a40
Shades <51397439+Shades4355@users.noreply.github.com>
2024-01-16 21:24:30 -0500

    2023 day9pt1 (#51)
    * day 9 pt 1 - files created
    
    * fixed naming convention
    
    * created get_input() function and test
    
    * created parse_input() function and test
    
    * created start() test; start() function is WIP
    
    * added find_next_line() function and tests
    
    * created find_number() function and test
    
    * added more find_number() tests
    
    * all tests pass
    
    * outputs the correct answer when real data used

---
+++ advent_2023/day_9/advent9pt1.py
@@ -0,0 +1,90 @@
+
+def find_next_line(old_line:list):
+    '''Takes in a list; outputs a list of the difference between each item as well as whether or not the list was all 0s'''
+    answer = []
+    zeros = 0
+
+    for i in range(0, len(old_line) - 1):
+        if not old_line[i] == 0:
+            new_num = old_line[i+1] - old_line[i]
+            answer.append(new_num)
+        elif old_line[i] == 0:
+            new_num = old_line[i+1] - old_line[i]
+            answer.append(new_num)
+            zeros += 1
+
+    if zeros == len(old_line) - 1:
+        return [True, answer]
+
+    return [False, answer]
+
+
+def find_number(file:list):
+    answer = 0
+    temp_list = []
+
+    for line in file:
+        temp_list.append(line)
+
+    for i in range(len(temp_list) - 1, 0, -1):
+        new_num = temp_list[i][-1] + temp_list[i - 1][-1]
+        temp_list[i - 1].append(new_num)
+
+    answer = temp_list[0][-1]
+
+    return answer
+
+
+def get_input(location:str):
+    '''parse txt file into a list'''
+    output_file = []
+
+    file = open(location, "r")
+
+    for line in file:
+        output_file.append(line.strip())
+
+    file.close()
+
+    return output_file
+
+
+def parse_input(file:list):
+    answer = []
+    str_answer = []
+
+    for i in range(0, len(file)):
+        answer.append([])
+        answer[i] = []
+        str_answer.append([])
+        str_answer[i] = file[i].split()
+        for num in str_answer[i]:
+            answer[i].append(int(num))
+
+    return answer
+
+
+def start(location:str):
+    answer = 0
+    file = parse_input(get_input(location))
+
+    for line in file:
+        all_zeros = False
+        new_list = []
+        new_line = line
+
+        # find all new lines
+        while not all_zeros:
+            new_list.append(new_line)
+            all_zeros, new_line = find_next_line(new_line)
+
+        # find last numbers, add them together
+        answer += find_number(new_list)
+
+    return answer
+
+
+#########
+# Start #
+#########
+print(start("day9input.txt"))
---
+++ advent_2023/day_9/day9_test.py
@@ -0,0 +1,112 @@
+import unittest
+from advent9pt1 import start as start_pt1, get_input as get_input_pt1, parse_input as parse_input_pt1, find_next_line, find_number
+
+class TestDay9(unittest.TestCase):
+    test_data = "day9input_test.txt"
+
+    def test_find_next_line(self):
+        def test_1(self):
+            prompt = [2, 4, 5, 2, 6]
+            answer = [False, [2, 1, -3, 4]]
+
+            return self.assertEqual(find_next_line(prompt), answer)
+        
+        def test_2(self):
+            prompt = [0, 0, 1, 0, 0]
+            answer = [False, [0, 1, -1, 0]]
+
+            return self.assertEqual(find_next_line(prompt), answer)
+        
+        def test_3(self):
+            prompt = [1, 0, 0, 1]
+            answer = [False, [-1, 0, 1]]
+
+            return self.assertEqual(find_next_line(prompt), answer)
+        
+        def test_4(self):
+            prompt = [7, 9]
+            answer = [False, [2]]
+            return self.assertEqual(find_next_line(prompt), answer)
+
+        def test_5(self):
+            prompt = [0, 0, 0, 0]
+            answer = [True, [0, 0, 0]]
+
+            return self.assertEqual(find_next_line(prompt), answer)
+        
+        def test_6(self):
+            prompt = [0]
+            answer = [True, []]
+        
+        # Tests for False return, plus new list
+        test_1(self)
+        test_2(self)
+        test_3(self)
+        test_4(self)
+
+        # Tests for True return, plus a list of 0s
+        test_5(self)
+        test_6(self)
+
+    def test_find_number(self):
+        def test_1(self):
+            file = [[1, 5, 9, 19],
+                [4, 7, 10],
+                [3, 3],
+                [0]]
+            answer = 32
+
+            return self.assertEqual(find_number(file), answer)
+        
+        def test_2(self):
+            file = [[1, 3, 6, 10, 15, 21],
+                    [2, 3, 4, 5, 6],
+                    [1, 1, 1, 1],
+                    [0, 0, 0]]
+            answer = 28
+
+            return self.assertEqual(find_number(file), answer)
+
+        def test_3(self):
+            file =[[10, 13, 16, 21, 30, 45],
+                   [3, 3, 5, 9, 15],
+                   [0, 2, 4, 6],
+                   [2, 2, 2],
+                   [0, 0]]
+            answer = 68
+
+            return self.assertEqual(find_number(file), answer)
+
+        test_1(self)
+        test_2(self)
+        test_3(self)
+
+    def test_get_input_pt1(self):
+        location = self.test_data
+        answer = ["0 3 6 9 12 15",
+                  "1 3 6 10 15 21",
+                  "10 13 16 21 30 45"]
+        
+        self.assertEqual(get_input_pt1(location), answer)
+
+    def test_parse_input_pt1(self):
+        file = ["0 3 6 9 12 15",
+                  "1 3 6 10 15 21",
+                  "10 13 16 21 30 45"]
+        answer = [[0, 3, 6, 9, 12, 15],
+                  [1, 3, 6, 10, 15, 21],
+                  [10, 13, 16, 21, 30, 45]]
+
+        self.assertEqual(parse_input_pt1(file), answer)
+
+    def test_start_pt1(self):
+        file = self.test_data
+        answer = 114
+
+        self.assertEqual(start_pt1(file), answer)
+
+#########
+# Start #
+#########
+if __name__ == "__main__":
+    unittest.main()
